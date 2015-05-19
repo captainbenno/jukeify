e2188b280f35b2319b93dff04733c50974ad93c1
Benjamin Dry <ben@dynamiccreative.com>
2015-05-19 22:20:39 +0930

    added facebook api test

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -10,6 +10,43 @@
 	<?php endif;?>
 
 	<br/><br/><a href="<?php echo site_url('/docs') ?>" class="btn btn-large btn-info">Browse the Docs</a>
+
+	<script>
+	  window.fbAsyncInit = function() {
+	    FB.init({
+	      appId      : '830654533679951',
+	      xfbml      : true,
+	      version    : 'v2.3'
+	    });
+	  };
+
+	  (function(d, s, id){
+	     var js, fjs = d.getElementsByTagName(s)[0];
+	     if (d.getElementById(id)) {return;}
+	     js = d.createElement(s); js.id = id;
+	     js.src = "//connect.facebook.net/en_US/sdk.js";
+	     fjs.parentNode.insertBefore(js, fjs);
+	   }(document, 'script', 'facebook-jssdk'));
+
+
+		FB.getLoginStatus(function(response) {
+		  if (response.status === 'connected') {
+		    console.log('Logged in.');
+		  }
+		  else {
+		    FB.login();
+		  }
+		});
+
+	</script>
+
+	<div
+	  class="fb-like"
+	  data-share="true"
+	  data-width="450"
+	  data-show-faces="true">
+	</div>
+
 </div>
 
 <hr />
